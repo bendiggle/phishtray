bd37c83d23c7516d6f6d8c758e8b6ba3cf105c2f
Feri Csepregi <feri@31.digital>
2018-12-03 03:08:27 +0000

    fixup! HEROKU - release-tasks and Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: ./release-tasks.sh
-web: gunicorn phishtray.wsgi --log-file -
+web: python ./manage.py collectstatic --noinput; gunicorn phishtray.wsgi --log-file -
--- release-tasks.sh
+++ release-tasks.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-echo "---- Preparing FE ------"
-cd frontend
-#yarn install
-yarn build
-
-echo "---- Updating static files ------"
-cd ..
-python manage.py collectstatic --noinput
-
 echo "---- Apply Migrations ------"
 python manage.py migrate
+
+
+echo "---- Check Static ------"
+python manage.py findstatic
+
+
