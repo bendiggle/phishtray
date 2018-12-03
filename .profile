c82ceaa4209bb3e46827a360ad8f73b40faa5558
Feri Csepregi <feri@31.digital>
2018-12-03 01:43:16 +0000

    fixup! HEROKU - release-tasks and Procfile

--- .profile
+++
@@ -1,3 +0,0 @@
-# Build FE files
-yarn install
-yarn build
--- release-tasks.sh
+++ release-tasks.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "---- Preparing FE ------"
-#cd frontend
-#yarn install
-#yarn build
+cd frontend
+yarn install
+yarn build
 
 echo "---- Updating static files ------"
-#cd ..
+cd ..
 python manage.py collectstatic --noinput
 
 echo "---- Migrate ------"
